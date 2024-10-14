ffc356a24c8a2349d86cd6feea87e80fb5fe042e
jeongjihun <wlgns2234@naver.com>
2024-10-14 18:55:02 +0900

    init: 프로젝트 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <div className={styles.header}></div>
+      <div className={styles.body}>freak blog</div>
+      <div className={styles.footer}></div>
+    </>
+  )
+}
+
+export default App

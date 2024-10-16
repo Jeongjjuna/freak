a0fc98fdafeb375b3bced39e0bcb73fde8ea38b5
jeongjihun <wlgns2234@naver.com>
2024-10-16 09:27:00 +0900

    feat: 모바일 footer 크기 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import styles from './Footer.module.css';
 
-function Footer(props) {
+function Footer() {
   return (
     <div className={styles.footer}>
       <ul className={styles.links}>
